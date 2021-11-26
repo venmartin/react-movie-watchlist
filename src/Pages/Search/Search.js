b3224bb4d4f8fe35eb97eb55d080c1037e6708da
Adam Martinovic <55118001+venmartin@users.noreply.github.com>
2021-11-26 14:07:37 +1100

    Added search form and styling

--- src/Pages/Search/Search.js
+++ src/Pages/Search/Search.js
@@ -1,9 +1,42 @@
 import React from 'react'
+import LoadingSpinner from '../../components/Loading'
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import SearchIcon from '@mui/icons-material/Search';
+import './Search.css'
+
+import { useState } from 'react';
+
+  
+
 
 const Search = () => {
+    const [type, setType] = useState()
+
+  // const [ loading, setLoading ] = useState(false)
+  // const [ page, setPage ] = useState(1)
+  // const [ content, setContent ] = useState([])
+  // const [ pageNum, setPageNum ] = useState()
+
+
   return (
     <div>
       <span className='pageTitle'>Search</span>
+      {/* <LoadingSpinner /> */}
+       
+        <div className='searchBox'>
+          <TextField 
+            id="filled-basic"
+            label="Search"
+            variant="filled"
+            style={{
+              flex: 1,
+             }}/>
+          <button className='searchBtn'>
+            <SearchIcon />
+          </button>
+        </div>
+        
     </div>
   )
 }
